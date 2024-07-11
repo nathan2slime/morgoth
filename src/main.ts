ec046d27c37f952cbed577a0731acbda4763c6e0
Jhonathan <nathan3boss@gmail.com>
2024-07-11 16:37:47 -0300

    feat: adding strategy for jwt validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { env } from '~/env';
 
 import { AppModule } from '~/app/app.module';
 
+import 'reflect-metadata';
+import { logger } from '~/logger';
+
 (async () => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT);
+  const app = await NestFactory.create(AppModule, {  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Morgoth')
+    .setDescription('Docs')
+    .setVersion('1.0')
+    .build();
+
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(env.PORT, () =>
+    logger.error('app running in http://localhost:'.concat(env.PORT)),
+  );
 })();
