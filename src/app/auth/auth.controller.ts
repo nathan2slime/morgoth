0dd430df012968be3cb7fea6935b3379a428f0e6
Jhonathan <nathan3boss@gmail.com>
2024-07-12 14:09:54 -0300

    feat: signin and signup controllers

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,17 +1,72 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from '~/app/auth/auth.service';
-import { SignUpDto } from '~/app/auth/auth.dto';
+import { SignInDto, SignUpDto } from '~/app/auth/auth.dto';
+import { JwtAuthGuard } from '~/app/auth/auth.guard';
+import { AUTH_COOKIE } from '~/constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() body: SignUpDto, @Res() res: Response) {
+  async signUp(
+    @Body() body: SignUpDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const data = await this.authService.signUp(body);
 
+    res.cookie(
+      AUTH_COOKIE,
+      {
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      },
+      { httpOnly: true },
+    );
+
     return res.status(HttpStatus.CREATED).json(data);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async auth(@Req() req: Request, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json(req.user);
+  }
+
+  @Get('refresh')
+  @UseGuards(AuthGuard('refresh'))
+  async refresh(@Req() req: Request, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json(req.user);
+  }
+
+  @Post('signin')
+  async signIn(
+    @Body() body: SignInDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const data = await this.authService.signIn(body);
+
+    res.cookie(
+      AUTH_COOKIE,
+      {
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      },
+      { httpOnly: true },
+    );
+
+    return res.status(HttpStatus.OK).json(data);
   }
 }
